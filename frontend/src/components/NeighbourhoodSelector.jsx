aa84b5807d7c550def043ec002f17e4615a11693
taielmatiasdeluca <taielmatiasdeluca@gmail.com>
2024-12-15 20:38:14 -0300

    se agrega el selection, de barrios

--- frontend/src/components/NeighbourhoodSelector.jsx
+++ frontend/src/components/NeighbourhoodSelector.jsx
@@ -1,14 +1,23 @@
 
 import { Label, Select } from "flowbite-react";
+import { useEffect, useState } from "react";
+import useNeightbourhood from "../hooks/useNeightbourhood";
+
 
 
 export const NeighbourhoodSelector = () =>{
-
-
+    const { neightbourhoods, loading } = useNeightbourhood();
+    console.log(loading)
     return (
         <div className="max-w-md">
             <Select id="neightbourhood" label="Selecciona tu barrio" placeholder="Selecciona tu barrio">
-           
+                { loading ? <option>Cargando...</option> :  neightbourhoods?.map(neighbourhood => (
+                    <option key={neighbourhood.hash} value={neighbourhood.hash}>
+                        {neighbourhood.name}
+                    </option>
+
+                    ))
+                }     
             </Select>
         </div>
     )
---
+++ frontend/src/hooks/useNeightbourhood.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import useApi from "./useApi";
+
+
+const useNeightbourhood = () => {
+
+    const [neightbourhoods, setNeightbourhoods] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const { api } = useApi();
+
+    const getNeightbourhoods = async () => {
+        setLoading(true);
+        setError(null);
+        try {
+            const response = await api.get("/neighbourhood");
+            setNeightbourhoods(response.data);
+
+            setLoading(false);
+        } catch (err) {
+            setLoading(false);
+            setError(err.message);
+        }
+    }
+
+    // Cargar socios al inicio
+    useEffect(() => {
+        getNeightbourhoods();
+    }, []);
+
+    return { neightbourhoods, loading, error, getNeightbourhoods };
+};
+
+export default useNeightbourhood;
